80cbe82cd0bb9988446a49e35734e20447d7c851
ilyaVahromeev <ilya.vahromeev@widestudio.ru>
2022-02-16 12:44:51 +0300

    fix forms reset

--- src/js/files/forms/forms.js
+++ src/js/files/forms/forms.js
@@ -1,6 +1,12 @@
 // Подключение функционала "Чертогов Фрилансера"
 // Вспомогательные функции
-import { isMobile, _slideUp, _slideDown, _slideToggle, FLS } from "../functions.js";
+import {
+	isMobile,
+	_slideUp,
+	_slideDown,
+	_slideToggle,
+	FLS,
+} from "../functions.js";
 // Модуль прокрутки к блоку
 import { gotoBlock } from "../scroll/gotoblock.js";
 // Класс select
@@ -10,8 +16,8 @@ import { SelectConstructor } from "../../libs/select.js";
 //==============================================================================================================================================================================================================================================================================================================================
 // Объект модулей форм для экспорта
 export const formsModules = {
-	selectModule: null
-}
+	selectModule: null,
+};
 //================================================================================================================================================================================================================================================================================================================================
 
 /*
@@ -33,35 +39,43 @@ data-goto-error - прокрутить страницу к ошибке
 
 // Работа с полями формы. Добавление классов, работа с placeholder
 export function formFieldsInit() {
-	const formFields = document.querySelectorAll('input[placeholder],textarea[placeholder]');
+	const formFields = document.querySelectorAll(
+		"input[placeholder],textarea[placeholder]"
+	);
 	if (formFields.length) {
-		formFields.forEach(formField => {
+		formFields.forEach((formField) => {
 			formField.dataset.placeholder = formField.placeholder;
 		});
 	}
 	document.body.addEventListener("focusin", function (e) {
 		const targetElement = e.target;
-		if ((targetElement.tagName === 'INPUT' || targetElement.tagName === 'TEXTAREA')) {
+		if (
+			targetElement.tagName === "INPUT" ||
+			targetElement.tagName === "TEXTAREA"
+		) {
 			if (targetElement.dataset.placeholder) {
-				targetElement.placeholder = '';
+				targetElement.placeholder = "";
 			}
-			targetElement.classList.add('_form-focus');
-			targetElement.parentElement.classList.add('_form-focus');
+			targetElement.classList.add("_form-focus");
+			targetElement.parentElement.classList.add("_form-focus");
 
 			formValidate.removeError(targetElement);
 		}
 	});
 	document.body.addEventListener("focusout", function (e) {
 		const targetElement = e.target;
-		if ((targetElement.tagName === 'INPUT' || targetElement.tagName === 'TEXTAREA')) {
+		if (
+			targetElement.tagName === "INPUT" ||
+			targetElement.tagName === "TEXTAREA"
+		) {
 			if (targetElement.dataset.placeholder) {
 				targetElement.placeholder = targetElement.dataset.placeholder;
 			}
-			targetElement.classList.remove('_form-focus');
-			targetElement.parentElement.classList.remove('_form-focus');
+			targetElement.classList.remove("_form-focus");
+			targetElement.parentElement.classList.remove("_form-focus");
 
 			// Моментальная валидация
-			if (targetElement.hasAttribute('data-validate')) {
+			if (targetElement.hasAttribute("data-validate")) {
 				formValidate.validateInput(targetElement);
 			}
 		}
@@ -71,10 +85,14 @@ export function formFieldsInit() {
 export let formValidate = {
 	getErrors(form) {
 		let error = 0;
-		let formRequiredItems = form.querySelectorAll('*[data-required]');
+		let formRequiredItems = form.querySelectorAll("*[data-required]");
 		if (formRequiredItems.length) {
-			formRequiredItems.forEach(formRequiredItem => {
-				if ((formRequiredItem.offsetParent !== null || formRequiredItem.tagName === "SELECT") && !formRequiredItem.disabled) {
+			formRequiredItems.forEach((formRequiredItem) => {
+				if (
+					(formRequiredItem.offsetParent !== null ||
+						formRequiredItem.tagName === "SELECT") &&
+					!formRequiredItem.disabled
+				) {
 					error += this.validateInput(formRequiredItem);
 				}
 			});
@@ -91,7 +109,10 @@ export let formValidate = {
 			} else {
 				this.removeError(formRequiredItem);
 			}
-		} else if (formRequiredItem.type === "checkbox" && !formRequiredItem.checked) {
+		} else if (
+			formRequiredItem.type === "checkbox" &&
+			!formRequiredItem.checked
+		) {
 			this.addError(formRequiredItem);
 			error++;
 		} else {
@@ -105,33 +126,39 @@ export let formValidate = {
 		return error;
 	},
 	addError(formRequiredItem) {
-		formRequiredItem.classList.add('_form-error');
-		formRequiredItem.parentElement.classList.add('_form-error');
-		let inputError = formRequiredItem.parentElement.querySelector('.form__error');
+		formRequiredItem.classList.add("_form-error");
+		formRequiredItem.parentElement.classList.add("_form-error");
+		let inputError =
+			formRequiredItem.parentElement.querySelector(".form__error");
 		if (inputError) formRequiredItem.parentElement.removeChild(inputError);
 		if (formRequiredItem.dataset.error) {
-			formRequiredItem.parentElement.insertAdjacentHTML('beforeend', `<div class="form__error">${formRequiredItem.dataset.error}</div>`);
+			formRequiredItem.parentElement.insertAdjacentHTML(
+				"beforeend",
+				`<div class="form__error">${formRequiredItem.dataset.error}</div>`
+			);
 		}
 	},
 	removeError(formRequiredItem) {
-		formRequiredItem.classList.remove('_form-error');
-		formRequiredItem.parentElement.classList.remove('_form-error');
-		if (formRequiredItem.parentElement.querySelector('.form__error')) {
-			formRequiredItem.parentElement.removeChild(formRequiredItem.parentElement.querySelector('.form__error'));
+		formRequiredItem.classList.remove("_form-error");
+		formRequiredItem.parentElement.classList.remove("_form-error");
+		if (formRequiredItem.parentElement.querySelector(".form__error")) {
+			formRequiredItem.parentElement.removeChild(
+				formRequiredItem.parentElement.querySelector(".form__error")
+			);
 		}
 	},
 	formClean(form) {
 		form.reset();
 		setTimeout(() => {
-			let inputs = form.querySelectorAll('input,textarea');
+			let inputs = form.querySelectorAll("input,textarea");
 			for (let index = 0; index < inputs.length; index++) {
 				const el = inputs[index];
-				el.parentElement.classList.remove('_form-focus');
-				el.classList.remove('_form-focus');
+				el.parentElement.classList.remove("_form-focus");
+				el.classList.remove("_form-focus");
 				formValidate.removeError(el);
-				el.value = el.dataset.placeholder;
+				el.placeholder = el.dataset.placeholder;
 			}
-			let checkboxes = form.querySelectorAll('.checkbox__input');
+			let checkboxes = form.querySelectorAll(".checkbox__input");
 			if (checkboxes.length > 0) {
 				for (let index = 0; index < checkboxes.length; index++) {
 					const checkbox = checkboxes[index];
@@ -139,10 +166,10 @@ export let formValidate = {
 				}
 			}
 			if (formsModules.selectModule) {
-				let selects = form.querySelectorAll('.select');
+				let selects = form.querySelectorAll(".select");
 				if (selects.length) {
 					for (let index = 0; index < selects.length; index++) {
-						const select = selects[index].querySelector('select');
+						const select = selects[index].querySelector("select");
 						formsModules.selectModule.selectBuild(select);
 					}
 				}
@@ -150,19 +177,21 @@ export let formValidate = {
 		}, 0);
 	},
 	emailTest(formRequiredItem) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
-	}
-}
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(
+			formRequiredItem.value
+		);
+	},
+};
 /* Отправка форм */
 export function formSubmit(validate) {
 	const forms = document.forms;
 	if (forms.length) {
 		for (const form of forms) {
-			form.addEventListener('submit', function (e) {
+			form.addEventListener("submit", function (e) {
 				const form = e.target;
 				formSubmitAction(form, e);
 			});
-			form.addEventListener('reset', function (e) {
+			form.addEventListener("reset", function (e) {
 				const form = e.target;
 				formValidate.formClean(form);
 			});
@@ -171,34 +200,40 @@ export function formSubmit(validate) {
 	async function formSubmitAction(form, e) {
 		const error = validate ? formValidate.getErrors(form) : 0;
 		if (error === 0) {
-			const ajax = form.hasAttribute('data-ajax');
-			if (ajax) { // Если режим ajax
+			const ajax = form.hasAttribute("data-ajax");
+			if (ajax) {
+				// Если режим ajax
 				e.preventDefault();
-				const formAction = form.getAttribute('action') ? form.getAttribute('action').trim() : '#';
-				const formMethod = form.getAttribute('method') ? form.getAttribute('method').trim() : 'GET';
+				const formAction = form.getAttribute("action")
+					? form.getAttribute("action").trim()
+					: "#";
+				const formMethod = form.getAttribute("method")
+					? form.getAttribute("method").trim()
+					: "GET";
 				const formData = new FormData(form);
 
-				form.classList.add('_sending');
+				form.classList.add("_sending");
 				const response = await fetch(formAction, {
 					method: formMethod,
-					body: formData
+					body: formData,
 				});
 				if (response.ok) {
 					let responseResult = await response.json();
-					form.classList.remove('_sending');
+					form.classList.remove("_sending");
 					formSent(form);
 				} else {
 					alert("Ошибка");
-					form.classList.remove('_sending');
+					form.classList.remove("_sending");
 				}
-			} else if (form.hasAttribute('data-dev')) { 	// Если режим разработки
+			} else if (form.hasAttribute("data-dev")) {
+				// Если режим разработки
 				e.preventDefault();
 				formSent(form);
 			}
 		} else {
 			e.preventDefault();
-			const formError = form.querySelector('._form-error');
-			if (formError && form.hasAttribute('data-goto-error')) {
+			const formError = form.querySelector("._form-error");
+			if (formError && form.hasAttribute("data-goto-error")) {
 				gotoBlock(formError, true, 1000);
 			}
 		}
@@ -206,11 +241,13 @@ export function formSubmit(validate) {
 	// Действия после отправки формы
 	function formSent(form) {
 		// Создаем событие отправки формы
-		document.dispatchEvent(new CustomEvent("formSent", {
-			detail: {
-				form: form
-			}
-		}));
+		document.dispatchEvent(
+			new CustomEvent("formSent", {
+				detail: {
+					form: form,
+				},
+			})
+		);
 		// Очищаем форму
 		formValidate.formClean(form);
 		// Сообщаем в консоль
@@ -235,9 +272,13 @@ export function formViewpass() {
 	document.addEventListener("click", function (e) {
 		let targetElement = e.target;
 		if (targetElement.closest('[class*="__viewpass"]')) {
-			let inputType = targetElement.classList.contains('active') ? "password" : "text";
-			targetElement.parentElement.querySelector('input').setAttribute("type", inputType);
-			targetElement.classList.toggle('active');
+			let inputType = targetElement.classList.contains("active")
+				? "password"
+				: "text";
+			targetElement.parentElement
+				.querySelector("input")
+				.setAttribute("type", inputType);
+			targetElement.classList.toggle("active");
 		}
 	});
 }
@@ -245,21 +286,24 @@ export function formViewpass() {
 export function formQuantity() {
 	document.addEventListener("click", function (e) {
 		let targetElement = e.target;
-		if (targetElement.closest('.quantity__button')) {
-			let value = parseInt(targetElement.closest('.quantity').querySelector('input').value);
-			if (targetElement.classList.contains('quantity__button_plus')) {
+		if (targetElement.closest(".quantity__button")) {
+			let value = parseInt(
+				targetElement.closest(".quantity").querySelector("input").value
+			);
+			if (targetElement.classList.contains("quantity__button_plus")) {
 				value++;
 			} else {
 				--value;
 				if (value < 1) value = 1;
 			}
-			targetElement.closest('.quantity').querySelector('input').value = value;
+			targetElement.closest(".quantity").querySelector("input").value =
+				value;
 		}
 	});
 }
 /* Модуь звездного рейтинга */
 export function formRating() {
-	const ratings = document.querySelectorAll('.rating');
+	const ratings = document.querySelectorAll(".rating");
 	if (ratings.length > 0) {
 		initRatings();
 	}
@@ -277,23 +321,23 @@ export function formRating() {
 
 			setRatingActiveWidth();
 
-			if (rating.classList.contains('rating_set')) {
+			if (rating.classList.contains("rating_set")) {
 				setRating(rating);
 			}
 		}
 		// Инициализайция переменных
 		function initRatingVars(rating) {
-			ratingActive = rating.querySelector('.rating__active');
-			ratingValue = rating.querySelector('.rating__value');
+			ratingActive = rating.querySelector(".rating__active");
+			ratingValue = rating.querySelector(".rating__value");
 		}
 		// Изменяем ширину активных звезд
 		function setRatingActiveWidth(index = ratingValue.innerHTML) {
 			const ratingActiveWidth = index / 0.05;
 			ratingActive.style.width = `${ratingActiveWidth}%`;
 		}
-		// Возможность указать оценку 
+		// Возможность указать оценку
 		function setRating(rating) {
-			const ratingItems = rating.querySelectorAll('.rating__item');
+			const ratingItems = rating.querySelectorAll(".rating__item");
 			for (let index = 0; index < ratingItems.length; index++) {
 				const ratingItem = ratingItems[index];
 				ratingItem.addEventListener("mouseenter", function (e) {
@@ -322,12 +366,12 @@ export function formRating() {
 			}
 		}
 		async function setRatingValue(value, rating) {
-			if (!rating.classList.contains('rating_sending')) {
-				rating.classList.add('rating_sending');
+			if (!rating.classList.contains("rating_sending")) {
+				rating.classList.add("rating_sending");
 
 				// Отправика данных (value) на сервер
-				let response = await fetch('rating.json', {
-					method: 'GET',
+				let response = await fetch("rating.json", {
+					method: "GET",
 
 					//body: JSON.stringify({
 					//	userRating: value
@@ -335,7 +379,6 @@ export function formRating() {
 					//headers: {
 					//	'content-type': 'application/json'
 					//}
-
 				});
 				if (response.ok) {
 					const result = await response.json();
@@ -349,11 +392,11 @@ export function formRating() {
 					// Обновление активных звезд
 					setRatingActiveWidth();
 
-					rating.classList.remove('rating_sending');
+					rating.classList.remove("rating_sending");
 				} else {
 					alert("Ошибка");
 
-					rating.classList.remove('rating_sending');
+					rating.classList.remove("rating_sending");
 				}
 			}
 		}
