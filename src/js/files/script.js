41cc90aef3c1343a7a3bff183d23f87b2ae58e41
Ilya Vakhromeev <vakhromeev_ilya@bk.ru>
2022-03-13 18:45:27 +0300

    made slider for mavery finally

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,5 +1,6 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-import { getHash,isMobile } from "./functions.js";
+import { getHash, isMobile } from "./functions.js";
+import "./sliderAdaptive.js";
 // import { formsModules } from "./forms/forms.js";
 let block = document.querySelector(".menu__item");
 //document.addEventListener("click", documentActions);
@@ -19,24 +20,24 @@ function _removeClasses(el, class_name) {
 }
 function createSlides() {
 	const sliders = document.querySelectorAll('.slider');
-	if(sliders.length > 0) {
-		sliders.forEach((slider,i) => {
+	if (sliders.length > 0) {
+		sliders.forEach((slider, i) => {
 			const images = document.createElement('div');
-			let   slides = [];
+			let slides = [];
 			let inputs = [];
 			let labels = [];
-			const links = slider.dataset.slider.replace(/\s{2,}/g, '').split(';',5);
+			const links = slider.dataset.slider.replace(/\s{2,}/g, '').split(';', 5);
 			links.forEach(link => {
 				slides.push(link);
 			});
-			slides.forEach((slide,index) => {
+			slides.forEach((slide, index) => {
 				const input = document.createElement('input');
 				input.type = 'radio';
 				input.name = `radio-btn-${i + 1}`;
-				input.id = `radio${index + 1}-${i+1}`;
+				input.id = `radio${index + 1}-${i + 1}`;
 				inputs.push(input);
 				const label = document.createElement('label');
-				label.setAttribute('for',`radio${index + 1}-${i+1}`);
+				label.setAttribute('for', `radio${index + 1}-${i + 1}`);
 				label.classList.add('navigation-btn');
 				labels.push(label);
 			});
@@ -46,150 +47,66 @@ function createSlides() {
 			labels.forEach(label => {
 				slider.append(label);
 			})
-			
+
 			images.classList.add('imgs_slides');
 		})
 	}
 }
-if(1) {
-	document.addEventListener('mouseover', (e) => {
-		const target = e.target;
-		if(target.classList.contains('card-preview-slider__item')) {
-			const span = target.querySelector('span');
-			const href = span.dataset.href ? span.dataset.href : 'https://mavery.ru/';
-			const images = target.closest('.card-preview-slider').querySelectorAll('img');
-			images.forEach((img,i) => {
-				img.classList.remove('_active');
-			})
-			target.closest('.card-preview-slider').querySelector(href).classList.add('_active');
-		} else if(target.closest('.card-preview-slider__item')) {
-			const href = target.dataset.href ? target.dataset.href : 'https://mavery.ru/';
-			const images = target.closest('.card-preview-slider').querySelectorAll('img');
-			images.forEach(img => {
-				img.classList.remove('_active');
-			})
-			target.closest('.card-preview-slider').querySelector(href).classList.add('_active');
-		} 
-	});
-	const cards = document.querySelectorAll('.card-preview-slider');
-	if(cards.length > 0) {
+const cards = document.querySelectorAll('.card-preview-slider');
+if (cards.length > 0) {
+	if (isMobile.any()) {
 		cards.forEach(card => {
-			card.addEventListener('mouseleave',(e) => {
-				const images = card.querySelectorAll('img');
-				images.forEach(img => {
-					img.classList.remove('_active');
-				});
-				card.querySelector('img:nth-of-type(1)').classList.add('_active');
+			const slider = card.querySelector('.swiper');
+			new Swiper(slider, {
+				slidePreview: 1,
+				loop: true,
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets',
+					dynamicBullets: true,
+				},
 			})
 		})
-	}
-}
-document.addEventListener('click', (e) => {
-	const targetElement = e.target;	
-	if(targetElement.matches('[id^="card-preview-slide"]') || targetElement.matches('.card-preview-slider__item')) {
-		const parent = targetElement.closest('.card-preview-slider');
-		window.location.href = parent.dataset.href;
-	}
-});
-if(isMobile.any()) {
-	const cards = document.querySelectorAll('.card-preview-slider');
-	if(cards.length > 0) {
+	} else {
+		document.addEventListener('mouseover', (e) => {
+			const target = e.target;
+			if (target.classList.contains('card-preview-slider__item')) {
+				const parent = target.closest('.card-preview-slider');
+				const slides = parent.querySelectorAll('.swiper-slide');
+				const lis = parent.querySelectorAll('.card-preview-slider__item');
+				lis.forEach((li, i) => {
+					if (target === li) {
+						li.classList.add('_active');
+						slides[i].classList.add('_active');
+					} else {
+						li.classList.remove('_active');
+						slides[i].classList.remove('_active');
+					}
+				})
+			}
+		});
+		document.addEventListener('click', (e) => {
+			const target = e.target;
+			if (target.classList.contains('card-preview-slider__item')) {
+				const parent = target.closest('.card-preview-slider');
+				const href = parent.dataset.href ? parent.dataset.href : 'https://www.google.com/';
+				window.location.href = href;
+			}
+		});
 		cards.forEach(card => {
-			const hammertime = new Hammer(card);
-			const rowSlider = card.querySelector('.card-preview-slider__row');
-			const images = Array.from(card.querySelectorAll('img'));
-			const dots = Array.from(card.querySelectorAll('.card-preview-slider__dots span'));
-			let activeIndex = 0;
-			rowSlider.addEventListener('transitionend', () => {
-				if(activeIndex == images.length - 2) {
-					rowSlider.classList.add('no-transition');
-					rowSlider.style.transform = `translate3d(0%,0,0)`;
-					images[0].classList.add('_active');
-					images[images.length - 1].classList.remove('_active');
-					setTimeout(function() {
-						rowSlider.classList.remove('no-transition');
-					},800);
-				}
-			})
-			hammertime.on('swiperight', function(ev) {	
-				images.some((img,i) => {	
-					if(img.classList.contains('_active')) {
-						activeIndex = i;
-						if(i > 0) {
-							img.classList.remove('_active');
-							img.previousElementSibling.classList.add('_active');
-						}
-						if(i == 0 ) {
-							img.classList.remove('_active');
-							card.querySelector('img:nth-last-of-type(1)').classList.add('_active');
-							return true;
-						}
-					}	
-				});
-				if(activeIndex == 0) {
-					rowSlider.style.transform = `translate3d(-${(images.length - 1) * 100}%,0,0)`;
-				} else {
-					rowSlider.style.transform = `translate3d(-${(activeIndex - 1) * 100}%,0,0)`;
-				}
-				dots.some((item,i,arr) => {
-					if(item.classList.contains('_active')) {
-						if(i == 0) {
-							item.classList.remove('_active');
-							arr[2].classList.add('_active');
-							return true;
-						}
-						if(i == 1) {
-							item.classList.remove('_active');
-							arr[0].classList.add('_active');
-							return true;
-						}
-						if(i == 2) {
-							item.classList.remove('_active');
-							arr[1].classList.add('_active');
-							return true;
-						}
+			card.addEventListener('mouseleave', (e) => {
+				const parent = e.currentTarget;
+				const slides = parent.querySelectorAll('.swiper-slide');
+				const lis = parent.querySelectorAll('.card-preview-slider__item');
+				lis.forEach((li, i) => {
+					if (i === 0) {
+						li.classList.add('_active');
+						slides[i].classList.add('_active');
+					} else {
+						li.classList.remove('_active');
+						slides[i].classList.remove('_active');
 					}
-				});
-			});
-			hammertime.on('swipeleft', function(ev) {
-				Array.from(images).some((img,i,arr) => {
-					if(img.classList.contains('_active')) {
-						activeIndex = i;
-						if(i + 1 != arr.length) {
-							img.classList.remove('_active');
-							img.nextElementSibling.classList.add('_active');
-							return true;
-						}
-						if(i + 1 == arr.length) {
-							img.classList.remove('_active');
-							card.querySelector('img').classList.add('_active');
-						}
-					}
-				});
-				if(activeIndex + 1 == images.length) {
-					rowSlider.style.transform = `translate3d(0%,0,0)`;
-				} else {
-					rowSlider.style.transform = `translate3d(-${(activeIndex + 1) * 100}%,0,0)`;
-				}
-				dots.some((item,i,arr) => {
-					if(item.classList.contains('_active')) {
-						if(i == 0) {
-							item.classList.remove('_active');
-							arr[1].classList.add('_active');
-							return true;
-						}
-						if(i == 1) {
-							item.classList.remove('_active');
-							arr[2].classList.add('_active');
-							return true;
-						}
-						if(i == 2) {
-							item.classList.remove('_active');
-							arr[0].classList.add('_active');
-							return true;
-						}
-					}
-				});
+				})
 			});
 		})
 	}
@@ -224,7 +141,7 @@ function documentActions(e) {
 			.querySelector(".header__form")
 			.classList.remove("_search-active");
 	}
-	if(targetElement.matches('[id^="card-preview-slide"]') || targetElement.matches('.card-preview-slider__item')) {
+	if (targetElement.matches('[id^="card-preview-slide"]') || targetElement.matches('.card-preview-slider__item')) {
 		const parent = targetElement.closest('.card-preview-slider');
 		window.location.href = parent.dataset.href;
 	}
